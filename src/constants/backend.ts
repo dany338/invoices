26a7c9f3cc26b19537e68f626162abc281208bfd
Daniel Andres Gallo Garcia <dany338@gmail.com>
2022-10-06 11:48:39 -0500

    first commit

---
+++ src/constants/backend.ts
@@ -0,0 +1,4 @@
+export const BACKEND_DEV = 'http://localhost:3002';
+export const BACKEND_URL = `${BACKEND_DEV}`;
+export const API_AUTH    = `${BACKEND_URL}/login`;
+export const API_INVOICE = `${BACKEND_URL}/invoices`;
